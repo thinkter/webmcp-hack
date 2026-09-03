c1989ead782cffa821a30250511acb68c45fda80
thinkter <ashmangamer0406@gmail.com>
2026-09-03 21:04:26 +0530

    refactor: clean up application entry point and remove obsolete CSS imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@xyflow/react/dist/style.css'
 import './styles.css'
-import './robust.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
